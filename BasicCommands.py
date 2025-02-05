5fac2f75641765c76b09690fe76b748ce59c86c2
Gk070 <geethulkrishna@gmail.com>
2025-02-05 18:36:33 +0530

    .

--- BasicCommands.py
+++ BasicCommands.py
@@ -1,3 +1,14 @@
+print(3*"Hi", end )
+
 print(9/2) # / will return 4.5
 
 print(9//2) # // will return only integer part
+
+# This is called parsing 
+num1 = input("Enter a number : ") # To read as string
+num2 = input("Enter a number : ")
+sum = num1 + num2 
+print(sum) # will return the numbers combined since concatination occurs(str + str)
+print(type(num1)) # type is used print the datatype of the variable
+
+num3 = int(input("Enter a number : ")) # To read as integer
--- pattern1.py
+++ pattern1.py
@@ -7,8 +7,8 @@ Pattern 1
 1 2 3 4
 ------------------- 
 '''
-n = int(input("Enter the number of rows : "))
+n = int(input("Enter the number of rows : ")) 
 for i in range(1, n + 1):
     for j in range(1, i + 1):
         print(j, end = " ")
-    print()
+    print() # for new line
