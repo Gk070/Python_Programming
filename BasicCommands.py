e6c88855a6caa6e474639879e89854a333c91543
Gk070 <geethulkrishna@gmail.com>
2025-02-05 19:44:41 +0530

    .

--- BasicCommands.py
+++ BasicCommands.py
@@ -1,4 +1,30 @@
-print(3*"Hi", end )
+import math # To import the module math 
+
+i = 0
+while(i < 3):
+    print(i, end = " ")
+    i += 1
+
+# printing the number in reverse order 
+for i in range(10, 1, -1):
+    print(i, end = " ")
+
+for i in range(1, 10, 2): # Range(1, 10, 2) means number from 1 - 10(excluding 10) and skipping 1 number
+    print(i, end = " ") 
+
+for i in range(1, 10): # Range(1, 10) means numbers from 1 - 10(excluding 10)
+    print(i, end=" ")
+
+for i in range(10): # To print first 9 numbers from 0 (Rangil kodukunnente 1 kurach loop execute avollu)
+    print(i, end = " ") 
+
+print(2 ** 3) # will print 2^3
+
+print(chr(65)) # chr 65 is used to covert ASCII value to character
+
+print(ord('A')) # ord 'A' to convert character to ASCII value
+
+print(3*"Hi\n") # To print a string 3 times '\n' is used to move to next line
 
 print(9/2) # / will return 4.5
 
@@ -11,4 +37,4 @@ sum = num1 + num2
 print(sum) # will return the numbers combined since concatination occurs(str + str)
 print(type(num1)) # type is used print the datatype of the variable
 
-num3 = int(input("Enter a number : ")) # To read as integer
+num3 = int(input("Enter a number : ")) # To read as integer 
---
+++ CheckPositiveOrNegative.py
@@ -0,0 +1,5 @@
+i = int(input("Enter a number : "))
+if(i > 0):
+    print(i, " is a positive number")
+else:
+    print(i, " is a negative number")
